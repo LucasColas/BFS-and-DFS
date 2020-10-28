3acfcc34718f424a0cab5758defb1d23c73de424
LucasColas <lucas.colas13@gmail.com>
2020-10-28 15:46:55 +0100

    update

--- BFS.py
+++ BFS.py
@@ -1,1 +1,14 @@
+Root = {'value': 1, 'depth':1}
 
+def Succ(node):
+    if node['value'] == 5:
+        return []
+
+    elif node['value'] == 4:
+        return [{'value':5, 'depth': node['depth']+1}]
+
+    else:
+        return [{'value': node['value']+1, \
+                 'depth':node['depth']+1}, \
+                 {'value':node['value']+2, \
+                 'depth':node['depth']+1}]
