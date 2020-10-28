1dbe7ccdfb0626adaedc86a0ddcd5fea7bb06570
LucasColas <lucas.colas13@gmail.com>
2020-10-28 17:12:26 +0100

    update

--- BFS.py
+++ BFS.py
@@ -2,9 +2,13 @@ from succ import Succ
 Root = {'value': 1, 'depth':1}
 def BFS_Tree(node):
     nodes = [node]
+    print(nodes)
     visited_nodes = []
+    i = 0
 
     while len(node) > 0:
+        print(i)
+        i += 1
         current_node = nodes.pop(0)
         visited_nodes.append(current_node)
         nodes.extend(Succ(current_node))
